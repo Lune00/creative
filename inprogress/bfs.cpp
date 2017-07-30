75e9a0b7bbc98b4d2d54a9a627447fe63b929e6c
paul schuhmacher - macbook <polynomial_c@hotmail.fr>
2017-07-30 18:09:25 +0200

    breadth first search ok

--- inprogress/bfs.cpp
+++ inprogress/bfs.cpp
@@ -1,3 +1,4 @@
+#include<iterator>
 #include<sstream>
 #include<cstdio>
 #include<iomanip>
@@ -21,23 +22,37 @@ struct Point{
   int ifrom;
   int jfrom;
 
+  bool w ;
+
 };
 
 bool operator==(const Point& a, const Point& b)
 {
   return ( a.i == b.i && a.j == b.j);
 }
+bool operator!=(const Point& a, const Point& b)
+{
+  return ( a.i != b.i || a.j != b.j);
+}
+
+
+//Ca marche toujours pas bien. On devrait avoir des diagonales. A mediter...
 
 int main(){
 
-  int nx = 3 ;
-  int ny = 3 ;
+  int nx = 40 ;
+  int ny = 40 ;
+
+  //On fixe un point de depart
+  Point start = { 2 , 25 , 2 , 25 , true};
+  Point end = { 33 , 34 , 33 , 33 , true};
 
   Point * grid = new Point [nx * ny];
   //contient les points de la frontiere
   std::vector<Point> frontier ;
   //contient les points deja visite
   std::vector<Point> came_from;
+
 
   //Initialise la grille ;
   for(unsigned int i = 0 ; i < nx ; i++)
@@ -46,10 +61,30 @@ int main(){
     {
       grid[ i * ny + j ].i = i ;
       grid[ i * ny + j ].j = j ;
+      grid[ i * ny + j ].w = true ;
     }
   }
-  //On fixe un point de depart
-  Point start = { 0 , 0 , 0 , 0 };
+
+  //Obstacle:
+  int obstracle = 0 ;
+  int width = 5 ;
+  for(unsigned int i = nx /4 - width  ; i < nx/4 + width ; i++)
+  {
+    for(unsigned int j = ny/4 -width ; j < ny/4 + width ; j++)
+    {
+      grid[ i * ny + j ].w = false ;
+      obstracle++;
+    }
+  }
+  for(unsigned int i = nx /2 - 2  ; i < nx/2 +2 ; i++)
+  {
+    for(unsigned int j = ny/2 -2 ; j < ny/2+2 ; j++)
+    {
+      grid[ i * ny + j ].w = false ;
+      obstracle++;
+    }
+  }
+
   //On initialise la frontiere
   frontier.push_back(start);
   came_from.push_back(start);
@@ -60,44 +95,45 @@ int main(){
 
   while(frontier.size() != 0 )
   {
-    cerr<<"Frontier size = "<<frontier.size()<<endl;
+    //cerr<<"Frontier size = "<<frontier.size()<<endl;
+
     //On prend le premier point de la frontiere
+
     Point current = frontier[0]; 
+
+   // cerr<<"Le point courrant est "<<current.i<<" "<<current.j<<endl;
     //On retire le point de la frontiere
     frontier.erase(frontier.begin());
 
     //contient les voisins de current
-    std::vector<Point> voisins;
+    std::vector<Point> voisins ; 
 
     int i = current.i ;
     int j = current.j ;
 
     //On prend ses voisin
-    if( (i-1) >= 0 && (i-1) < nx ) {
-      Point voisin = { i-1 , j } ;
-      voisins.push_back(voisin);
-    }
-
-    if( (i+1) >= 0 && (i+1) < nx ) {
-      Point voisin = { i+1 , j } ;
-      voisins.push_back(voisin);
-    }
-
-    if( (j+1) >= 0 && (j+1) < ny ) {
-      Point voisin = { i , j+1 } ;
-      voisins.push_back(voisin);
-    }
-
-    if( (j-1) >= 0 && (j-1) < ny ) {
-      Point voisin = { i , j-1 } ;
-      voisins.push_back(voisin);
-    }
-
-    cerr<<"Le point "<<i<<" "<<j<<" a "<<voisins.size()<<" voisins:"<<endl;
-    for(std::vector<Point>::iterator it = voisins.begin(); it != voisins.end(); it++)
+    for(int k = i - 1 ; k < i + 2 ; k++  )
     {
-      cerr<<it->i<<" "<<it->j<<endl;
+      for(int l = j - 1 ; l < j + 2 ; l++  )
+      {
+
+	if ( (k == i && l == j) || (grid[ k * ny + l].w == false) ) continue ;
+
+	if ( (k >= 0 && k < nx ) && ( l >= 0 && l < ny ) ) 
+	{
+	  Point p = { k , l } ;
+	  voisins.push_back(p);
+	}
+
+      }
     }
+
+  //  cerr<<"Le point "<<i<<" "<<j<<" a "<<voisins.size()<<" voisins:"<<endl;
+
+  //  for(std::vector<Point>::iterator it = voisins.begin(); it != voisins.end(); it++)
+  //  {
+  //    cerr<<it->i<<" "<<it->j<<endl;
+  //  }
 
     //On parcourt les voisins
     for(std::vector<Point>::iterator it = voisins.begin(); it != voisins.end(); it++)
@@ -107,16 +143,15 @@ int main(){
       for(std::vector<Point>::iterator it2 = came_from.begin(); it2 != came_from.end(); it2++)
       {
 	if( *it == *it2){
-	  cerr<<"Ce point a deja ete visite, on passe au suivant."<<endl;
+	  //cerr<<"Ce point a deja ete visite, on passe au suivant."<<endl;
 	  alreadyvisited = true ;
 	  break;
 	}
       }
 
       if(alreadyvisited) continue; 
-      //S'il n'a pas ete visite on l'ajoute a la frontiere en lui associant le point d ou on vient
-      cerr<<"Process."<<endl;
 
+      //S'il n'a pas ete visite on l'ajoute a la frontiere en lui associant le point d ou on vient
       it->ifrom = current.i ;
       it->jfrom = current.j ;
 
@@ -126,12 +161,68 @@ int main(){
   }
 
   cerr<<"Nombre de points visites : "<<came_from.size()<<endl;
+  cerr<<"Taille obstacle : "<<obstracle<<endl;
+  
+  //On met a jour la grille
+  cerr<<"Mise a jour de la grille."<<endl;
+  for(std::vector<Point>::iterator it = came_from.begin() ; it != came_from.end() ; it++)
+  {
+      grid[ it->i * ny + it->j].ifrom = it->ifrom ; 
+      grid[ it->i * ny + it->j].jfrom = it->jfrom ; 
+  }
+
+  //On trace le chemin du depart a la destination:
+  cerr<<"Calcul du chemin entre start et end."<<endl;
+
+  std::vector<Point> path ;
+
+  path.push_back(grid[end.i * ny + end.j]);
+  std::vector<Point>::iterator it = path.begin();
+
+  while( *it != start)
+  {
+
+    Point next = grid[it->ifrom * ny + it->jfrom];
+    cerr<<"Point suivant :" <<next.i<<" "<<next.j<<endl;
+    cerr<<"Ce point est lie a :" <<next.ifrom<<" "<<next.jfrom<<endl;
+    path.push_back(next);
+    cerr<<"Path.size : "<<path.size()<<endl;
+    //Next element
+    it = path.begin() + (path.size()- 1);
+  }
+
+  cerr<<"Nombre de points a parcourir pour arriver a destination : "<<path.size()<<endl;
+  
+  //On print les paths
+  ofstream opath("paths.txt");
+  ofstream grille("grid.txt");
+  ofstream lechemin("chemin.txt");
 
   //A la fin pour chaque point on a le point d'ou il vient
   for(std::vector<Point>::iterator it2 = came_from.begin(); it2 != came_from.end(); it2++)
   {
-    cerr<<it2->i<<" "<<it2->j<<" attache a "<<it2->ifrom<<" "<<it2->jfrom<<endl;
+    double s = 1. ;
+   // cerr<<it2->i<<" "<<it2->j<<" attache a "<<it2->ifrom<<" "<<it2->jfrom<<endl;
+    opath<<it2->i+0.5<<" "<<(it2->j)+0.5<<" "<<(it2->ifrom -it2->i)*s<<" "<<(it2->jfrom - it2->j)*s<<endl;
   }
+  opath.close();
+
+  for(unsigned int i = 0 ; i < nx ; i++)
+  {
+    for(unsigned int j = 0 ; j < ny ; j++)
+    {
+      if(grid[i*ny+j].w) grille<<grid[ i * ny + j ].i+0.5<<" "<<grid[ i * ny + j ].j+0.5<<endl ;
+    }
+  }
+  grille.close();
+
+  for(std::vector<Point>::iterator it2 = path.begin(); it2 != path.end(); it2++)
+  {
+    //cerr<<it2->i<<" "<<it2->j<<endl;
+    lechemin<<it2->i+0.5<<" "<<it2->j+0.5<<endl;
+  }
+
+  lechemin.close();
 
   delete [] grid;
 
