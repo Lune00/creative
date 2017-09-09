d6e4c01da03c1fd482f89d6c6a94f6744871f6a1
paul schuhmacher - macbook <polynomial_c@hotmail.fr>
2017-09-10 01:05:15 +0200

    script bash pour constituer base de donnees pour poesie

--- learnbash/inputoutput.sh
+++ learnbash/inputoutput.sh
@@ -2,6 +2,43 @@
 
 #Reference a la base de donnees(bdd)
 bdd="poesielibrairie.txt"
+#Tableau de phonetique voyelle et consonnes
+#Note : le • alt+maj+point represente la phoneme "on"
+phon_table=('a' 'i' 'y' 'u' 'o' 'O' 'e' 'E' '°' '2' '9' '5' '1' '@' '•' '3' 'j' '8' 'w' 'p' 'b' 't' 'd' 'k' 'g' 'f' 'v' 's' 'z' 'Z' 'm' 'n' 'N' 'I' 'R' 'x' 'G' 'S' 'l')
+nature_table=('nom' 'adj' 'ver')
+
+#Fonction qui teste si la phonetique correspond aux symboles (pas d erreur)
+#Prend comme argument la phonetique du mot $1
+#On construit un tableau vide
+#Si chaque caractere correspond a un phoneme de la table on rajoute un 0
+#Si la taille du tableau de verite est egale a celle du phoneme en entree c ok
+check_phonetique(){
+  verite=()
+  for i in $(seq 1 ${#1})
+  do
+    phoneme=${phonetique:$i-1:1}
+    #Check si le phoneme  $i existe dans phon_table
+    for j in "${phon_table[@]}"
+    do
+      if [ $phoneme == $j ];then
+	verite+="0"
+	break
+      fi
+    done
+  done
+  echo ${#verite}
+}
+
+#Verifie si la nature du mot existe
+check_nature(){
+  for i in "${nature_table[@]}"
+  do
+    if [ $1 == $i ];then
+      echo "found"
+      break
+    fi
+  done
+}
 
 #On test si la base existe, si elle n'existe pas, on la cree.
 if [ -f "$bdd" ];then
@@ -9,6 +46,98 @@ if [ -f "$bdd" ];then
 else
   echo "La base de donnees $bdd n'a pas encore ete cree."
   touch $bdd
-  echo "La bae de donnees $bdd a ete cree."
+  echo "La base de donnees $bdd a ete cree."
 fi
+
+taillebase=`awk 'END{print NR}' $bdd`
+echo "La base contient $taillebase mots."
+
+#Debut boucle d'ajout a la librairie
+while [ -z $reponse ] || [ "$reponse" != "x" ]; do
+  while [ -z $wordexist ] || [ $wordexist != "0" ];do
+    echo""
+    echo "->Nouvelle entrée"
+    echo""
+    read -p "Entrez le mot à ajouter a la base de données: " mot
+    #On teste si le mot n'existe pas deja dans la base
+    wordexist=$(grep -ci $mot $bdd)
+    if [ $wordexist != "0" ];then
+      echo "Le mot $mot existe déjà dans la base."
+    fi
+  done
+  wordexist=""
+
+  #Traitement de l'entree:
+
+  #Phonetique + test:
+  echo ""
+  phonetique_t=""
+  while [ "$phonetique_t" != ${#phonetique} ];do
+    read -p "* Phonetique du mot $mot: " phonetique
+    phonetique_t=$(check_phonetique $phonetique) 
+  done
+
+  #Nature du mot: 3 lettres (adj,nom,verb...)
+  echo ""
+  found=""
+  while [ "$found" != "found" ];do
+    read -p "* Nature du mot $mot: " nature
+    found=$(check_nature $nature)
+  done
+
+  #Genre:m,f,n a checker
+  echo""
+  read -p "* Genre du mot $mot (m,f,n): " genre
+  #Accord:s,p,i,n a checker
+  echo""
+  read -p "* Accord du mot $mot (s,p,i,n): " accord
+  #Nombre de syllabes
+  echo""
+  read -p "* Nombre de syllabes du mot $mot: " nsyll
+  #Registre checker
+  echo""
+  read -p "* Registre du mot $mot: " registre
+
+  #Synonymes 
+  synonymes=()
+  echo "Donnez des synonymes du mot $mot:"
+  read -a synonymes
+
+  #Related
+  related=()
+  echo "Donnez des mots associes au mot $mot:"
+  read -a related
+
+  #Format de sortie
+  echo ""
+  echo "Résumé"
+  echo ""
+
+  echo "--------------------------------------"
+  echo "Mot: $mot"
+  echo "Phonétique: $phonetique"
+  echo "Nature: $nature"
+  echo "Accord: $accord"
+  echo "Genre: $genre"
+  echo "Nbsyllabes: $nsyll"
+  echo "Registre: $registre"
+  echo "Synonymes: ${synonymes[*]}"
+  echo "Associés: ${related[*]}"
+  echo "--------------------------------------"
+
+  #Recapitulatif et demande de confirmation
+
+  #Ecriture sortie
+
+  echo "Le mot $mot a ete ajouté a la base de données."
+  read -p "Tapez x pour arrêter la saisie." reponse
+done
+
+
+echo "Base de données mise a jour."
+
+
+#On pourra implementer des petits scripts qui donne une analyse base de donnes
+#Combien de mots masculins, feminins, qui terminent en a, de verbe, le nombre de mots par syllabes
+#Faire un script qui imprime un resume de la base de donnees
 
--- learnbash/tipslearned.sh
+++ learnbash/tipslearned.sh
@@ -31,10 +31,55 @@ pour afficher le premier element
 echo ${tableau[*]} 
 pour afficher tous les elements
 
-#-Existence fichier
+# Les conditions
+if [ TEST ];then
+  blabla
+elif [ test ];then
+  blabla
+else
+  blabla
+fi
+
+Ne pas oublier les espaces a l interieur des crochets!!!
+
+#Trois types de test en bash : test sur chaines de caracteres, test sur des nombres, test sur des fichiers
+
+if [ -z $texte ] : verifier si la chaine est vide
+
+Pour comparer des nombres il faut utiliser -eq -ne -gt -ge ...
+Enfin les tests sur les fichiers -e -d ...
+
+if [ -d $fichier ];then 
+  verifie que le fichier est un repertoire (un repertoire est un fichier, un fichier n  est pas necessairemnet un repertoire)
+  -f verifier si le fichier est un fichier (et non un repertoire et s il existe)
+ex:
+
 if [ -f "cheminverslefichier" ];then
   echo "Le fichier existe";
 fi
 
-#Les conditions
+#Tester si une expression est trouvee dans un fichier
+grep -ci expression fichier
+le -c renvoie le nombre de fois l expression est trouvee au lieu de la ligne
+le -i ignore la casse
+
+#do wile
+while [ test ]; do
+  blablabla
+done
+
+#Fonctions 
+print_something(){
+  echo "Hello $1"
+}
+appel de la fonction print_something Toi
+le $1 refere au premier argument passe a la fonction apres son appel
+Le return d une fonction en bash n est pas pousse, on renvoie un nombre qu il faudrai interpreter ensuite (0 en general tout va bien, sinon ca va pas)
+
+print_something(){
+  echo "Hello $1"
+  return 5;
+}
+On peut recuperer la derniere valeur retournee par le dernier appel de fonction grace a l expression $?
+
 
