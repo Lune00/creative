196850bf7ff8a2ccc39f30eb196db7593d3db509
paul schuhmacher - macbook <polynomial_c@hotmail.fr>
2017-09-13 01:30:53 +0200

    makereport updated + aligned output

--- library/makereport.sh
+++ library/makereport.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 source libconfig.sh
 
-
+echo ""
 echo " -- ${red}Rapport${green} sur${yellow}${bold} $formatedlib ${reset} --"
 
 #Nombre de noms
 
+#Themes : nombre et occurences
 
-#Nombre de theme
 themes=$(awk 'BEGIN{FS="\t";} {print $12}' $formatedlib)
-
+themes=$(gawk 'BEGIN{ RS=";|\\s" ; ORS=" "}{ print $0}' <<< $themes)
+declare -A asar
+for i in ${themes}; do asar[$i]=$(( ${asar[$i]}+1 )); done
+themes=$(gawk 'BEGIN{RS="\\s" ; ORS=" "}{ if(var[$0]==0){var[$0]+=1; print $0}}' <<< "$themes")
+echo -e "\nListe des thèmes présents:\n"
+for i in ${themes}
+do
+  printf "${yellow}%-10s${reset}\tNombre d'entrées: ${green}%3s${reset}\n" "$i" "${asar[$i]}"
+done
 
 
 #Mots synonymes/related qui n'ont pas encore d'entree
