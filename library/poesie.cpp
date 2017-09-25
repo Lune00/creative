6795e3a8e6663722f8023c94de6768588681914d
paul schuhmacher - macbook <polynomial_c@hotmail.fr>
2017-09-25 15:23:23 +0200

    Constructeur mot en cours

--- library/poesie.cpp
+++ library/poesie.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<fstream>
 
-#define library "mylib.txt"
+#define mylibrary "mylibrary.txt"
 
 using namespace std;
 
@@ -10,45 +11,67 @@ class Mot{
 
   private:
 
-  string mot_;
-  string lemme_;
-  string phon_;
-  string grammar_;
-  string infover_;
+    string mot_;
+    string lemme_;
+    string phon_;
+    string grammar_;
+    string infover_;
 
-  char genre_;
-  char accord_;
-  int nsyll_;
+    char genre_;
+    char accord_;
+    int nsyll_;
 
-  std::vector<string> registre_;
-  std::vector<string> syn_;
-  std::vector<string> ass_;
-  std::vector<string> themes_;
+    std::vector<string> registre_;
+    std::vector<string> syn_;
+    std::vector<string> ass_;
+    std::vector<string> themes_;
 
   public:
-  Mot();
-  ~Mot();
+    Mot();
+    Mot(string);
+    ~Mot();
 };
 
 Mot::Mot(){};
+
+Mot::Mot(string entree)
+{
+
+  cerr<<"Init mot"<<endl;
+
+
+
+};
+
 Mot::~Mot(){};
 
 
-class Librairie{
-
-  private:
-    std::vector<Mot> corpus_;
-  public:
-    Librairie();
-    ~Librairie();
-};
-
-Librairie::Librairie(){};
-Librairie::~Librairie(){};
+//class Librairie{
+//  private:
+//    std::vector<Mot> noms_;
+//    std::vector<Mot> adjectifs_;
+//    std::vector<Mot> verbes_;
+//  public:
+//    Librairie();
+//    ~Librairie();
+//};
+//
+//Librairie::Librairie(){};
+//Librairie::~Librairie(){};
 
 
 int main(){
 
   cout<<"Bonjour poésie."<<endl;
+  std::vector<Mot> corpus;
+  ifstream mylib(mylibrary);
 
+  while(mylib){
+    string entree;
+    getline(mylib,entree);
+    //Chaque entree (entree) de la librairie est contenue dans entree
+    //Il faut maintenant la splitter pour en faire un mot, constructeur mot
+    Mot mot = Mot(entree);
+    corpus.push_back(mot);
+  };
 }
