7a1991056b8b1987c6655c007db0511b464bf8fd
paul schuhmacher - macbook <polynomial_c@hotmail.fr>
2017-09-28 23:45:43 +0200

    reorgranisation poesie/article

--- library/poesie.cpp
+++ library/poesie.cpp
@@ -6,6 +6,9 @@
 
 #define mylibrary "mylibrary.txt"
 
+using namespace std;
+
+
 
 //Les doublons sont gérés (normalement) par la librairie elle-meme.
 //Inutile donc en pratique de devoir traiter le cas des doublons ici.
@@ -22,7 +25,6 @@
 //Ne pas oublier de partir du titre (un nom + un adjectif + un registre + un theme)
 //Chercher synonymes, lies, et random
 
-using namespace std;
 
 
 // Global functions
@@ -41,7 +43,7 @@ vector<std::string> parsestring(string stringtoparse, string delimiter)
   }
   //On rajoute la derniere entree (ce qu'il reste)
   tokens.push_back(stringtoparse);
-  
+
   //On retire les caracteres vides (dus au formatage en bash)
   //On place tous les caracteres vides a la fin de string et on se place au premier caractere vide
   //puis on supprime jusqu'a la fin (algorithm)
@@ -79,20 +81,14 @@ class Mot{
     ~Mot();
     string getmot() {return mot_;}
     string getgenre() {return genre_;}
+    string getaccord() {return accord_;}
     vector<string> getgrammar() { return grammar_;}
     string getlastphoneme() {char last = phon_.back(); string last_s(1,last); return last_s;}
-    static const string phon_table[];
-    static const string grammar_table[];
 };
-
-
-const string Mot::phon_table[]={
-"a","i","y","u","o","O","e","E","°","2","9","5","1","@","§","3","j","8","w","p","b","t","d","k","g","f","v","s","z","Z","m","n","N","I","R","x","G","S","l"};
-const string Mot::grammar_table[]={"NOM","VER","ADJ"};
 
 Mot::Mot(){};
 
-//Constructeur d'un mot avec une entree de la bibliotheque
+//Constructeur d'un mot avec une entree de la bib
 //L'ordre des champs est donne par le formatage de la librairie (libconfig.sh)
 //Il doit y avoir 12 champs!
 Mot::Mot(string entree)
@@ -130,33 +126,62 @@ Mot::Mot(string entree)
 
 Mot::~Mot(){};
 
+//Articles definition: ms fs mfp
+//On va ranger des attributs et des fonctions generales dans une classe
+class bib
+{
+  public:
+    bib();
+
+    //Variables:
+    static const string phon_table[];
+    static const string grammar_table[];
+    static const string definis[];
+    static const string indefinis[];
+    //Fonctions:
+    static bool check_phonetique(string);
+    static bool check_grammar(string);
+    static vector<Mot> return_grammar_liste(vector<Mot>&,string);
+    static vector<Mot> return_last_phon_liste(vector<Mot>&,string);
+    static Mot randomMot(vector<Mot>&);
+    static string returnArticle(Mot&,string);
+};
+
+const string bib::phon_table[]={
+  "a","i","y","u","o","O","e","E","°","2","9","5","1","@","§","3","j","8","w","p","b","t","d","k","g","f","v","s","z","Z","m","n","N","I","R","x","G","S","l"};
+
+const string bib::grammar_table[]={"NOM","VER","ADJ"};
+
+//0 masculin singulier/ 1 feminin singulier / 2 pluriel
+const string bib::definis[]= {"le","la","les"};
+const string bib::indefinis[]= {"un","une","des"};
+
 //Checker la phonetique pour une phoneme donee (la phonetique de la librairie est deja checkee par la librairie)
 //en comparant a phon_table (39 phonemes, a verifier)
-bool check_phonetique(string phoneme){
+bool bib::check_phonetique(string phoneme){
   bool isfound = false;
   for(unsigned int i = 0 ; i != 39 ; i++)
   {
-    if ( phoneme == Mot::phon_table[i] ) { isfound = true; break ;}
+    if ( phoneme == bib::phon_table[i] ) { isfound = true; break ;}
   }
   return isfound;
 }
 
 //Check classe grammaticale
-bool check_grammar(string grammar){
+bool bib::check_grammar(string grammar){
   bool isfound = false;
   for(unsigned int i = 0 ; i != 3 ; i++)
   {
-    if ( grammar == Mot::grammar_table[i] ) { isfound = true; break ;}
+    if ( grammar == bib::grammar_table[i] ) { isfound = true; break ;}
   }
   return isfound;
 }
 
-
 //Fonction qui renvoie un vecteur de mots qui se terminent par la phoneme phoneme
-vector<Mot> return_last_phon_liste(vector<Mot>& corpus, string phoneme)
+vector<Mot> bib::return_last_phon_liste(vector<Mot>& corpus, string phoneme)
 {
   vector<Mot> liste;
-  if( !check_phonetique(phoneme) ) return liste;
+  if( !bib::check_phonetique(phoneme) ) return liste;
   for(vector<Mot>::iterator it = corpus.begin(); it != corpus.end() ; it++){
     if( it->getlastphoneme() == phoneme) liste.push_back(*it);
   }
@@ -164,10 +189,10 @@ vector<Mot> return_last_phon_liste(vector<Mot>& corpus, string phoneme)
 }
 
 //Renvoie les mots d'une classe grammaticale donnee
-vector<Mot> return_grammar_liste(vector<Mot>& corpus, string grammar)
+vector<Mot> bib::return_grammar_liste(vector<Mot>& corpus, string grammar)
 {
   vector<Mot> liste;
-  if( !check_grammar(grammar) ) return liste;
+  if( !bib::check_grammar(grammar) ) return liste;
   for(vector<Mot>::iterator it = corpus.begin(); it != corpus.end() ; it++){
     //Recupere vector grammar
     vector<string> vgrammar = it->getgrammar();
@@ -189,27 +214,23 @@ void affiche_mots(vector<Mot>& liste){
 }
 
 //Renvoie un mot au hasard dans un vecteur de mot
-Mot randomMot(vector<Mot>& liste){
+//WARNING pas un bon hasard (provisoire)
+Mot bib::randomMot(vector<Mot>& liste){
   int randomIndex = rand () % liste.size() ;
   return liste[randomIndex];
 }
 
-//Gere article en genre/nombre
-//Partitif, défini, indéfini
-//Demonstratif ...
-//Ou faire une simple fonction plutot qui encapsule tout le travail!
-//Ou une classe abstraite, ou une fonction de Mot directement
-class Article{
 
-  private:
-
-
-  public:
-
-};
-
-
-
+//Prend le mot et le type (défini, indéfini, partitif, démonstratif...)
+// Defini: le la les
+// Indefini: un une des
+// Partitif: du de la des
+//Si defini, regarde premiere lettre du mot, si voyelle ou h eliser
+string bib::returnArticle(Mot& mot, string type){
+  if(mot.getgrammar() != "NOM") return string();
+  //A completer
+  return strin();
+}
 // MAIN
 
 int main(){
@@ -234,13 +255,13 @@ int main(){
   //Tests:
 
   //vector<Mot> liste = return_last_phon_liste(corpus,"@");
-  vector<Mot> liste_ver = return_grammar_liste(corpus,"VER");
-  vector<Mot> liste_nom = return_grammar_liste(corpus,"NOM");
-  vector<Mot> liste_adj = return_grammar_liste(corpus,"ADJ");
+  vector<Mot> liste_ver = bib::return_grammar_liste(corpus,"VER");
+  vector<Mot> liste_nom = bib::return_grammar_liste(corpus,"NOM");
+  vector<Mot> liste_adj = bib::return_grammar_liste(corpus,"ADJ");
 
   //affiche_mots(liste);
-  Mot nom = randomMot(liste_nom);
-  Mot adj = randomMot(liste_adj);
+  Mot nom = bib::randomMot(liste_nom);
+  Mot adj = bib::randomMot(liste_adj);
 
   //Faire une classe qui gere les articles (fem/mas/demonstratifs/L' au lieu de l'...)
   //IL prend en entree la Mot (genre, le nombre et premiere lettre) et la nature(défini par l'utilisateur)
