1771d44148c803e61acee0075295a8cd1f10b2e2
paul schuhmacher - macbook <polynomial_c@hotmail.fr>
2017-10-05 11:58:12 +0200

    correction bug - up lib

--- library/makereport.sh
+++ library/makereport.sh
@@ -39,18 +39,22 @@ rsyn=$(gawk 'BEGIN{ RS=";|\\s" ; ORS=" "}{ print $0}' <<< $rsyn)
 rsyn=$(gawk 'BEGIN{RS="\\s" ; ORS=" "}{if(var[$0]==0){var[$0]+=1; print $0}}' <<< "$rsyn")
 
 awaiters=""
+
+#Il faut supprimer les doublons en regardant s'ils ont déja une entrée
+
 #On check si ils ont deja une entree dans la librairie
 #Les doublons seront geres par addawaiters et non ici
 for i in ${rsyn}
 do
-  #found=$(awk -v var="$i" '$1==var' "$formatedlib")
-  #if [ -z "$found" ]; then
+  found=$(awk -v var="$i" '$1==var' "$formatedlib")
+  if [ -z "$found" ]; then
     awaiters+=$i" "
-  #fi
+  fi
 done
 
 awaiters="${awaiters%?}"
 #On ecrit les awaiters de la librairie formatee dans un fichier awaiters.txt
+> "$waitlib"
 for i in ${awaiters}
 do
   #Check si pas deja present dans le fichier
@@ -67,4 +71,4 @@ echo -e ".Mots en attente ajoutés depuis le dernier rapport (${green}${#add_tab
 
 echo ""
 enattente=(`cat "$waitlib"`)
-echo -e ".Liste des mots en attente:\n ${enattente[@]}"
+echo -e "${green}.Liste des mots en attente:${reset}\n ${enattente[@]}"
