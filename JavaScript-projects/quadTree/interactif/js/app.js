e938578d4040af08084f910c6eb37e8a27f37868
Lune00 <paul.schuhm@gmail.com>
2020-04-30 17:34:45 +0200

    init demo03 project for collisions - wip

--- JavaScript-projects/quadTree/interactif/js/app.js
+++ JavaScript-projects/quadTree/interactif/js/app.js
@@ -1,7 +1,7 @@
 //Init all canvas in their own namespace
 
 //Start demo01 and init its UI
-// let p5demo01 = new p5(demo01,'canvas-demo01-insert-querry');
-// uiApp01.init();
+let p5demo01 = new p5(demo01,'canvas-demo01-insert-querry');
+uiApp01.init();
 
-let p5demo03 = new p5(demo03,'canvas-demo03-collisions');
+// let p5demo03 = new p5(demo03,'canvas-demo03-collisions');
