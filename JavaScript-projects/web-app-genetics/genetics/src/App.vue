aad92f4fa7bc0e6dfcedda1edb999ed7216e87e3
pschuhmacher <paul.schuhmacher@studio-goliath.com>
2021-02-08 09:59:45 +0100

    update

--- JavaScript-projects/web-app-genetics/genetics/src/App.vue
+++ JavaScript-projects/web-app-genetics/genetics/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App : Genetics" />
+    <AddAbstractFeature></AddAbstractFeature>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AddAbstractFeature from "@/views/AddAbstractFeature.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    AddAbstractFeature
   }
 };
 </script>
---
+++ JavaScript-projects/web-app-genetics/genetics/src/views/AddAbstractFeature.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <div>
+    <form
+      id="add_abstract_feature"
+      @submit.prevent="checkForm"
+      action="#"
+      method="post"
+    >
+    <p>
+    <input
+      type="submit"
+      value="Submit"
+    >
+  </p>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+        
+    };
+  },
+  methods: {
+    checkForm: function (e) {
+      console.log("check form", e);
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
