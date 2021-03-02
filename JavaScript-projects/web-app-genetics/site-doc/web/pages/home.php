1a448e7907ab445b47afcd5588b88adf8c03d7b4
pschuhmacher <paul.schuhmacher@studio-goliath.com>
2021-03-02 12:07:26 +0100

    ok, y a plus qua

--- JavaScript-projects/web-app-genetics/site-doc/web/pages/home.php
+++ JavaScript-projects/web-app-genetics/site-doc/web/pages/home.php
@@ -1,16 +1,5 @@
+<?php require_once('content/model-preview.php') ?>
 
+<h2 id="les-modèles-de-données">Les Modèles de données</h2>
 
-<h2 id="les-modèles-de-données">Les modèles de données</h2>
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php require_once('content/data-structures/gene-toolkit.php') ?>
